cecd605003db55df065f7248cf4b6b708eea94c8
flov3rh <floperh@gmail.com>
2020-01-22 14:14:59 -0500

    Move sessions logic from the view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
-  include SessionsHelper
+  helper_method :log_in, :current_user, :log_out, :logged_in?
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
