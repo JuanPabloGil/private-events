9c9f40e6a4f717d8388f62de5009038ec7bcdf21
flov3rh <floperh@gmail.com>
2020-01-23 16:22:56 -0500

    Work on the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_202127) do
+ActiveRecord::Schema.define(version: 2020_01_23_210941) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "event_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "user_events", force: :cascade do |t|
