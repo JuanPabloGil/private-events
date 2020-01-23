01898a8fcc2af40599a71cbecb1bff87bb38900d
flov3rh <floperh@gmail.com>
2020-01-23 16:59:08 -0500

    Create show for EventController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,10 @@ class EventsController < ApplicationController
     end
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   private
     def events_params
       params.require(:event).permit(:event_date, :description)
