3a574a9518d99c7df9272f80cd81b60776e2086b
flov3rh <floperh@gmail.com>
2020-01-23 16:40:48 -0500

    Generate EventsController

---
+++ app/controllers/events_controller_controller.rb
@@ -0,0 +1,10 @@
+class EventsControllerController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'events_controller/new'
+  get 'events_controller/show'
+  get 'events_controller/all'
   get 'sessions/new'
   root 'main#index'
   get  '/signup',  to: 'users#new'
