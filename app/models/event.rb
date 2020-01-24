8b67827cfa6f454d844f6bff0ec532fed35e2216
flov3rh <floperh@gmail.com>
2020-01-24 16:27:47 -0500

    Validate presence of event_date for Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
   has_many :user_events
   has_many :users, through: :user_events, source: :user
   validates :desc, presence: true, length: { minimum: 8}
+  validates :event_date, presence: true
   scope :upcoming, -> { where("event_date >= ?", Time.zone.now) }
   scope :past, -> { where("event_date < ?", Time.zone.now) }
 end
