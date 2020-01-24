3defae8b05142e8db4983859399eb64477291d63
flov3rh <floperh@gmail.com>
2020-01-24 16:19:09 -0500

    Create tests for Event model

---
+++ spec/models/event_rspec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.describe Event, type: :model do
+  let(:user) { User.new(name: "user1", email: "user1@example.com") }
+  let(:event) { Event.new(desc: "desc", event_date: "1980/10/10") }
+
+  it "Checks if Event model is valid" do
+    user.save
+    event.creator_id = user.id
+    expect(event).to be_valid
+  end
+
+  context "Checking for the Event validations" do
+
+    it "Checks if the event description is valid " do
+      event.desc = ""
+      user.save
+      event.creator_id = user.id
+      expect(event).not_to be_valid
+    end
+
+    it "Checks if the event description is too short " do
+      event.desc = "aa"
+      user.save
+      event.creator_id = user.id
+      expect(event).not_to be_valid
+    end
+
+    it "Checks if the email is present " do
+      user.email = ""
+      user.save
+      event.creator_id = user.id
+      expect(event).not_to be_valid
+    end
+
+    it "Checks if the event date its prescent " do
+      event.date = ""
+      user.save
+      event.creator_id = user.id
+      expect(event).not_to be_valid
+    end
+  end
+end
