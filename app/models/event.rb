27a4f91c604126f1211fc3b9ee5bf81c6367aa18
flov3rh <floperh@gmail.com>
2020-01-24 16:23:17 -0500

    Add validation for minimun size on Event desc

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_many :user_events
   has_many :users, through: :user_events, source: :user
-  validates :desc, presence: true
+  validates :desc, presence: true, length: { minimum: 8}
   scope :upcoming, -> { where("event_date >= ?", Time.zone.now) }
   scope :past, -> { where("event_date < ?", Time.zone.now) }
 end
