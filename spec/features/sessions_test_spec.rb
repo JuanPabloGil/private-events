1987889bf353c4184856160adfa2fdfb123640d5
Flover Herrera <floperh@gmail.com>
2020-01-28 16:22:50 -0500

    Add more session tests

--- spec/features/sessions_test_spec.rb
+++ spec/features/sessions_test_spec.rb
@@ -11,3 +11,22 @@ require 'rails_helper'
       expect(page).to have_content("Log in")
     end
   end
+
+  describe "", :type => :feature do
+    it 'Logs in an user' do
+      User.create(name: 'Juan Pablo', email: 'jpablomgil@email.com')
+      visit   '/sessions/new'
+      fill_in('session_email', with: 'jpablomgil@email.com')
+      click_button 'Log in'
+      expect(page).to have_content("Juan Pablo")
+    end
+  end
+
+  describe "GET '/events/new'", type: :feature do
+    it 'Non user creating event' do
+      visit('/events/new')
+      expect(page).to have_content("You must be logged in first")
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Log in")
+    end
+  end
